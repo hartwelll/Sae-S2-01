967c79e507faa7e9264339954f57bb179627df3c
mdubois <marinedbs80@gmail.com>
2025-06-05 09:46:31 +0200

    Inventaire fonctionnel pour l'instant : carré jaune dans le ciel à modifier par image niveau sol, et texte dans l'inventaire à changer par image.

--- src/main/java/universite_paris8/iut/ylecoguic/saeterrarialike/modele/Inventaire.java
+++ src/main/java/universite_paris8/iut/ylecoguic/saeterrarialike/modele/Inventaire.java
@@ -1,7 +1,16 @@
 package universite_paris8.iut.ylecoguic.saeterrarialike.modele;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Inventaire {
+    private final ObservableList<Objet> objets = FXCollections.observableArrayList();
 
-
-
+    public ObservableList<Objet> getObjets() {
+        return objets;
+    }
+    public void addObjet(Objet objet) {
+        objets.add(objet);
+    }
 }
