a5f5b66235f8878f4c11fe0bf65cd324a59cb5c4
mdubois <marinedbs80@gmail.com>
2025-06-05 09:54:41 +0200

    Inventaire fonctionnel pour l'instant : carré jaune dans le ciel à modifier par image niveau sol, et texte dans l'inventaire à changer par image.

---
+++ src/main/java/universite_paris8/iut/ylecoguic/saeterrarialike/vue/VueObjet.java
@@ -0,0 +1,23 @@
+package universite_paris8.iut.ylecoguic.saeterrarialike.vue;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import universite_paris8.iut.ylecoguic.saeterrarialike.modele.Objet;
+
+public class VueObjet extends Rectangle {
+    private final Objet objet;
+
+    public VueObjet(Objet objet, double x, double y, double width, double height) {
+        super(width, height);
+        this.objet = objet;
+        setTranslateX(x);
+        setTranslateY(y);
+        setFill(Color.GOLD);
+        setStroke(Color.BLACK);
+        setPickOnBounds(true);
+    }
+
+    public Objet getObjet() {
+        return objet;
+    }
+}
