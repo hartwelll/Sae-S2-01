352facc729a36cf17d5dacfa2e2185999013645a
mdubois <marinedbs80@gmail.com>
2025-06-05 09:46:31 +0200

    affiche d'un coeur pour leurs placement, tableview pour voir où inventaire

--- src/main/java/universite_paris8/iut/ylecoguic/saeterrarialike/modele/Objet.java
+++ src/main/java/universite_paris8/iut/ylecoguic/saeterrarialike/modele/Objet.java
@@ -1,15 +1,27 @@
 package universite_paris8.iut.ylecoguic.saeterrarialike.modele;
 
 public class Objet {
+    private String nom;
+    private String desc;
 
-    private String nomOb;
-
-    public Objet (String nomOb){
-        this.nomOb = nomOb;
+    public Objet(String nom, String desc) {
+        this.nom = nom;
+        this.desc = desc;
     }
 
-    public String getNomOb() {
-        return nomOb;
+    public String getNom() {
+        return nom;
     }
 
+    public String getDesc() {  // ✅ Le getter était déjà bon
+        return desc;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
